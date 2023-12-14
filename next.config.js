0bf1beb719d712b9722c9e3439a2fd23d8f3c882
Coding Hindi <84448185+codinghindi@users.noreply.github.com>
2023-12-14 19:17:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
-const path = require('path')
- 
+// next.config.js
+
+const path = require('path');
+
 module.exports = {
+  basePath: '/HelloCMS', // Change 'your-repo-name' to your actual repository name
+  assetPrefix: '/HelloCMS/', // Change 'your-repo-name' to your actual repository name
+  target: 'experimental-serverless-trace',
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      };
+    }
+    return config;
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+};
