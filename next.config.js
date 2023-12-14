093c68d9fdf32f3a08d852eb19abf984bc23b04b
Coding Hindi <84448185+codinghindi@users.noreply.github.com>
2023-12-14 19:42:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Perform any customizations to webpack configuration here
+    
+    // For example, if you want to add a loader for a specific file type (e.g., SVG)
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+  // Other configurations...
+};
