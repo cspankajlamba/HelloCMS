2aae8dff1793a0a75dbbd93f786f2ec2f7690e61
Coding Hindi <84448185+codinghindi@users.noreply.github.com>
2023-12-14 19:59:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
 module.exports = {
-  webpack: (config) => {
-    return config;
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+ webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      })
+
+      return config
+    },
   // Other configurations...
 };
